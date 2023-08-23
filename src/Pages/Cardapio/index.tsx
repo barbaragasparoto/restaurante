568209cf424cf2b58f36af28f1751dd87cb139b1
barbaragasparoto <barbaragasparotofigueiredo@gmail.com>
2023-08-23 08:57:29 -0300

    aula do dia 23/08

--- src/Pages/Cardapio/index.tsx
+++ src/Pages/Cardapio/index.tsx
@@ -1,11 +1,19 @@
 import styles from './Cardapio.module.scss';
-import Logo from '../../assets/logo.svg';
+import Logo from 'assets/logo.svg';
 
 export default function Cardapio() {
     return (
         <main>
             <nav className={ styles.menu }>
                <img src={Logo} alt="Logo do Restaurante" />
+            <header className={styles.header}> 
+            <div className={styles.header__text}>
+                "A casa do código e da massa!!"
+            </div>
+            </header>
+            <section>
+                <h3>Cardápio</h3>
+            </section>
             </nav>
         </main>
     );
